8cd4bad16cec892b6c45cf978d149c5d3c842c51
🜲 PaPaGuy Codes <175421249+papaguycodes@users.noreply.github.com>
2024-11-18 14:39:06 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to ReactJS</h1>
+        <p>This is a sample ReactJS application template.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
